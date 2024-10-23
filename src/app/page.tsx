27a955d354043c2fcf0e1010b90ac9149608172e
eddymafin <marumai2451@gmail.com>
2024-10-23 18:50:56 +0900

    いったんコミット

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,70 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default function Page() {
+  const [categories, setCategories] = useState([]); // カテゴリーを管理するための状態
+  const [selectVal, setSelectedVal] = useState("");
+  const [imageUrl, setImageUrl] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch("https://dog.ceo/api/breeds/list/all");
+        if (!res.ok) {
+          throw new Error("リストの取得に失敗しました");
+        }
+        const data = await res.json();
+        console.log(data);
+        const breeds = data.message;
+        const categoriesList = Object.keys(breeds);
+        setCategories(categoriesList);
+      } catch (error) {
+        console.error("エラーです:", error);
+      }
+    }
+    fetchData();
+  }, []);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleSelectChange = async (e) => {
+    const category = e.target.value;
+    setSelectedVal(category);
+    if (category) {
+      const response = await fetch(
+        `https://dog.ceo/api/breed/${category}/images/random`
+      );
+      const data = await response.json();
+      console.log(data);
+      console.log(data.message);
+      setImageUrl(data.message);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-3 bg-green-100 min-h-screen">
+      <div className="mt-10 max-w-[30%] w-full flex flex-col gap-3">
+        <p className="text-center text-lg">Choose your doggy </p>
+        <select className="border p-2" onChange={handleSelectChange}>
+          {categories.map((post) => (
+            <option value={post} key={post.id}>
+              {post}
+            </option>
+          ))}
+        </select>
+        <p className="text-center text-2xl font-bold mb-2">
+          Your choice doggy<span className="ml-2 ">{selectVal}</span>
+        </p>
+        <div className="flex items-center justify-center overflow-hidden h-full">
+          <Image
+            src={imageUrl}
+            alt={selectVal}
+            className="w-full object-cover  max-w-[18.75rem] max-h-[18.75rem]"
+            width={200}
+            height={200}
+            loading="lazy"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {selectVal && <p className="text-center">You like it?</p>}
+      </div>
     </div>
   );
 }
