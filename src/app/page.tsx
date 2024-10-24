ee77d206f2f03abf90733d97851632a8e17b8cf1
eddymafin <marumari2451@gmail.com>
2024-10-24 12:11:37 +0900

    犬の表示を複数表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 export default function Page() {
   const [categories, setCategories] = useState<string[]>([]); // カテゴリーを管理するための状態
   const [selectVal, setSelectedVal] = useState("");
+  const [lists, setList] = useState([]);
   const [imageUrl, setImageUrl] = useState("");
   useEffect(() => {
     async function fetchData() {
@@ -14,7 +15,7 @@ export default function Page() {
           throw new Error("リストの取得に失敗しました");
         }
         const data = await res.json();
-        console.log(data);
+        // console.log(data);
         const breeds = data.message;
         const categoriesList = Object.keys(breeds);
         setCategories(categoriesList);
@@ -40,19 +41,29 @@ export default function Page() {
     const category = e.target.value;
     setSelectedVal(category);
     if (category) {
+      // const response = await fetch(
+      //   `https://dog.ceo/api/breed/${category}/images/random`
+      // );
       const response = await fetch(
-        `https://dog.ceo/api/breed/${category}/images/random`
+        `https://dog.ceo/api/breed/${category}/images`
       );
-      const data = await response.json();
-      console.log(data);
-      console.log(data.message);
-      setImageUrl(data.message);
+      const dogData = await response.json();
+      const array = dogData.message;
+      console.log(array);
+      setImageUrl(dogData.message[0]);
+      setList(array.slice(0, 20));
     }
+  };
+
+  // 配列をシャッフルする関数
+  const shuffleArray = () => {
+    const shuffledList = [...lists].sort(() => Math.random() - 0.5); // 配列をランダムにシャッフル
+    setList(shuffledList); // シャッフルした配列をセット
   };
 
   return (
     <div className="flex flex-col justify-center items-center gap-3 bg-green-100 min-h-screen bg-[url('/dog.svg')] bg-repeat">
-      <div className="mt-10 max-w-[30%] w-full flex flex-col gap-3">
+      <div className="py-20 max-w-[90%] md:max-w-[80%] w-full flex flex-col gap-3">
         <p className="text-center text-lg">Choose your doggy </p>
         <select className="border p-2" onChange={handleSelectChange}>
           {categories.map((post, index) => (
@@ -64,26 +75,44 @@ export default function Page() {
         <p className="text-center text-2xl font-bold mb-2">
           Your choice doggy<span className="ml-2 ">{selectVal}</span>
         </p>
-        <div className="flex items-center justify-center overflow-hidden h-full">
+        <ul className="grid grid-cols-2 md:grid-cols-4 gap-2 mt-4">
+          {lists.map((list, index) => (
+            <li
+              key={index}
+              className="flex items-center justify-center md:max-w-[18.75rem]  border-none overflow-hidden bg-white"
+            >
+              <Image
+                src={list}
+                alt={`Dog ${index}`}
+                className="w-auto object-contain h-[9.375rem]"
+                width={150}
+                height={150}
+                loading="lazy"
+              />
+            </li>
+          ))}
+        </ul>
+        {selectVal && (
+          <p className="text-center font-bold text-lg">
+            Did you find your favorite doggy?
+          </p>
+        )}
+        <button
+          onClick={shuffleArray}
+          className="bg-green-900 text-white p-3 rounded-md w-full md:max-w-[21.875rem] mx-auto"
+        >
+          Want see more pics?
+        </button>
+        {/* <div className="flex items-center justify-center overflow-hidden h-full">
           <Image
             src={imageUrl}
             alt={selectVal}
-            className="w-full object-cover  max-w-[18.75rem] max-h-[18.75rem] border-none"
+            className="w-full object-cover  md:max-w-[18.75rem] md:max-h-[18.75rem] border-none"
             width={200}
             height={200}
             loading="lazy"
           />
-        </div>
-        {selectVal && <p className="text-center">You like it?</p>}
-
-        {/* <Image
-          src="dog.svg"
-          alt={selectVal}
-          className="w-full object-cover  max-w-[18.75rem] max-h-[18.75rem]"
-          width={200}
-          height={200}
-          loading="lazy"
-        /> */}
+        </div> */}
       </div>
     </div>
   );
