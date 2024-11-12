94e8012d5752d3b6042e670122d1971db4eaa059
eddymafin <marumari2451@gmail.com>
2024-11-12 10:58:57 +0900

    ページネーションをコンポーネント化

--- src/app/component/Pagination.tsx
+++ src/app/component/Pagination.tsx
@@ -2,31 +2,40 @@ import { Dispatch, SetStateAction } from "react";
 
 interface Props {
   lists: Array<string[]>;
-  currentNumber: Dispatch<SetStateAction<number>>;
+  currentNumber: number; // 現在のページ番号を受け取る
+  setCurrentNumber: Dispatch<SetStateAction<number>>;
 }
 
-export default function Pagination({ lists, currentNumber }: Props) {
-  const buttonCommonClass = `font-bold text-lg`;
-  console.log(currentNumber);
+export default function Pagination({
+  lists,
+  currentNumber,
+  setCurrentNumber,
+}: Props) {
+  const buttonCommonClass = `font-bold text-lg disabled:opacity-30`;
+  console.log(lists);
   const changePagination = (e: React.MouseEvent<HTMLButtonElement>) => {
     // number型に変更
-    return currentNumber(Number(e.currentTarget.value));
+    return setCurrentNumber(Number(e.currentTarget.value));
   };
   const prevButton = () => {
-    // if (currentNumber === 0) return;
-    currentNumber((prevState) => {
-      return prevState - 1;
+    if (currentNumber === 0) return;
+    setCurrentNumber((currentNumber) => {
+      return currentNumber - 1;
     });
   };
   const nextButton = () => {
-    // if (currentPage === lists.length - 1) return;
-    currentNumber((prevState) => {
-      return prevState + 1;
+    if (currentNumber === lists.length - 1) return;
+    setCurrentNumber((currentNumber) => {
+      return currentNumber + 1;
     });
   };
   return (
     <div className="grid grid-cols-[1fr,auto,1fr] gap-3 justify-center mt-5 items-center">
-      <button className={`${buttonCommonClass}`} onClick={prevButton}>
+      <button
+        className={buttonCommonClass}
+        disabled={currentNumber === 0}
+        onClick={prevButton}
+      >
         Prev
       </button>
       <ul className="flex gap-3 justify-center flex-wrap items-center">
@@ -34,7 +43,9 @@ export default function Pagination({ lists, currentNumber }: Props) {
           return (
             <li key={index}>
               <button
-                className={` w-[3.125rem] h-[3.125rem] flex items-center justify-center rounded-full  text-white md:hover:bg-green-600`}
+                className={`${
+                  currentNumber === index ? "bg-green-600" : "bg-green-900"
+                } w-[2.5rem] h-[2.5rem] md:w-[3.125rem] md:h-[3.125rem] flex items-center justify-center rounded-full  text-white md:hover:bg-green-600`}
                 value={index}
                 onClick={changePagination}
               >
@@ -44,7 +55,11 @@ export default function Pagination({ lists, currentNumber }: Props) {
           );
         })}
       </ul>
-      <button className={`${buttonCommonClass}`} onClick={nextButton}>
+      <button
+        className={buttonCommonClass}
+        disabled={currentNumber === lists.length - 1}
+        onClick={nextButton}
+      >
         Next
       </button>
     </div>
